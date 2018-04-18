b336c528d375c06a1833ae913dadd8ae2bf6d1e1
daniel <daniel@daniel-VirtualBox>
2018-04-18 16:55:24 +0200

    2.2 (versión segunda prov)

--- mio_clon/src/mio_clon/Mio_clon.java
+++ mio_clon/src/mio_clon/Mio_clon.java
@@ -16,6 +16,7 @@ public class Mio_clon {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo, como estas?");
+        System.out.println("Reguleras");
     }
     
 }
