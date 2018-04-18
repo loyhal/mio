283e2af6f3187f8dce55d817d91cbc3abbb3bbe1
daniel <daniel@daniel-VirtualBox>
2018-04-18 16:47:47 +0200

    1.2 (versión primera)

--- mio_clon/src/mio_clon/Mio_clon.java
+++ mio_clon/src/mio_clon/Mio_clon.java
@@ -15,7 +15,7 @@ public class Mio_clon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo, como estas?");
     }
     
 }
